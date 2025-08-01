e133cc7d227585c72268aa9c07a781450dcdd602
Andrash Yang <cocoychris@gmail.com>
2025-08-01 23:31:41 +0800

    `PrismaErrorCode` type corrected

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 import { PrismaError } from "prisma-error-enum";
 
+type ValueOf<T> = T[keyof T];
+
 /**
  * Prisma 中的最大整數值
  */
@@ -8,5 +10,5 @@ export const MAX_PRISMA_INT = 2147483647;
 /**
  * Prisma 錯誤代碼枚舉
  */
-export type PrismaErrorCode = keyof typeof PrismaError;
+export type PrismaErrorCode = ValueOf<typeof PrismaError>;
 export { PrismaError as PrismaErrorCode } from "prisma-error-enum";
